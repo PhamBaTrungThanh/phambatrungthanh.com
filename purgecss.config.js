f1221917997cc18d0d1604458f98a5b5e30c4932
Arthur <phambatrungthanh@gmail.com>
2020-01-28 09:16:05 +0000

    Downgrade prugecss version

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     content: ["./_site/**/*.html"],
-    css: ["./_site/css/site.css"],
+    css: ["./_site/assets/css/tailwind.css"],
+    // Tailwind CSS config
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 }
